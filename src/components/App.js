63ca31a2e7c61d6f77ad1da4ba67909e4055b24a
Abdelrahmen Bentahar <abdelrahmenbentahar@gmail.com>
2024-03-02 22:56:18 +0100

    Première parti du projet cv

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,9 @@
-import logo from '../media/logo.svg';
-import '../style/css/App.css';
+import "../style/css/App.css";
+
+import Quisuisje from "./Quisuisje";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Quisuisje />;
 }
 
 export default App;
---
+++ src/components/Quisuisje.js
@@ -0,0 +1,35 @@
+function Quisuisje() {
+  return (
+    <section className="container-fluid">
+      <div className="row">
+        <div className="col-md-6 col-12 d-flex justify-content-center">
+          <div className="img">
+            <img
+              src="https://img.freepik.com/photos-gratuite/jeune-homme-barbu-chemise-rayee_273609-5677.jpg?w=1380&t=st=1707738165~exp=1707738765~hmac=78bd6c84a0519c03a8ed08774419f6bd86d59664bf3c4b0e69385cc229406a6f"
+              alt="portrait"
+            />
+          </div>
+        </div>
+        <div className="col-md-6 col-12 d-flex">
+          <p>
+            Pellentesque felis tortor, mollis nec augue eget, hendrerit ornare
+            elit. Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. Vestibulum sit amet enim orci.
+            Aenean non libero dui. Integer vitae mollis risus. Ut congue urna
+            quis odio dapibus auctor. Vivamus sit amet magna nec ligula bibendum
+            pellentesque sit amet non velit. Duis pellentesque, urna sit amet
+            mattis elementum, justo sapien viverra magna, quis condimentum nibh
+            odio non tellus. Donec erat nisi, laoreet in arcu non, molestie
+            commodo ex. Donec sed mattis orci. Morbi eu aliquet augue. Aenean
+            dignissim nibh nunc, ut consequat augue viverra et. Duis a ligula
+            vitae ipsum laoreet sollicitudin vel eget lorem. Suspendisse
+            dignissim in turpis in elementum. Ut egestas porttitor ipsum in
+            tristique. In rhoncus egestas magna vitae accumsan.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Quisuisje;
